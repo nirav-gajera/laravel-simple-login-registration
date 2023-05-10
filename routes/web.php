de874bf1ad3c8619284a47a47aef1d10800ae004
Nirav Gajera <127472114+nirav-gajera@users.noreply.github.com>
2023-05-10 09:24:04 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,17 +12,21 @@ use App\Http\Controllers\Auth\AuthController;
 |
 */
 
+//login
 Route::get('login', 'auth\AuthController@index')->name('login');
 Route::post('post-login', 'auth\AuthController@postLogin')->name('login.post');
 
+//registration
 Route::get('registration', 'auth\AuthController@registration')->name('register');
 Route::post('post-registration','auth\AuthController@postRegistration')->name('register.post');
 
+//dashboard
 Route::get('dashboard', 'auth\AuthController@dashboard'); 
 
+//logout
 Route::get('logout', 'auth\AuthController@logout')->name('logout');
 
-
+//welcome
 Route::get('/', function () {
     return view('welcome');
 });
